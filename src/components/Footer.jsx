2b9676eb011a6a118dc64d47543f208cade04174
jaybee4real <okorojijaybee1@gmail.com>
2020-11-29 09:30:57 +0100

    _home page design in progress

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-container">
+                <div className="row">
+                    <div className="logo"></div>
+                </div>
+            </div>
+        )
+    }
+}
